8074700b8d2b9d9110bd9b8f114631d5f36aa18a
SumedhChobitkar <158187477+SumedhChobitkar@users.noreply.github.com>
2025-10-30 13:30:39 +0530

    Add files via upload

---
+++ DigitalClassRoomManagement/src/main/java/com/DigitalClassRoomManagement/DigitalClassRoomManagementApplication.java
@@ -0,0 +1,13 @@
+package com.DigitalClassRoomManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalClassRoomManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalClassRoomManagementApplication.class, args);
+	}
+
+}
---
+++ DigitalClassRoomManagement/src/test/java/com/DigitalClassRoomManagement/DigitalClassRoomManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DigitalClassRoomManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalClassRoomManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
