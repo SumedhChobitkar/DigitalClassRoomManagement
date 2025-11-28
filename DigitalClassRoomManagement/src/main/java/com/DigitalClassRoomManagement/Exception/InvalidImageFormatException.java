6aa6a1601f6554ce17c8e93509b19aed0064919b
nupurmeghal <nupurmeghal3@gmail.com>
2025-11-28 16:08:59 +0530

    I have added InvalidImageFormatException class for teacher dashboard profile controller

---
+++ DigitalClassRoomManagement/src/main/java/com/DigitalClassRoomManagement/Exception/InvalidImageFormatException.java
@@ -0,0 +1,7 @@
+package com.DigitalClassRoomManagement.Exception;
+
+public class InvalidImageFormatException extends RuntimeException{
+    public InvalidImageFormatException(String message) {
+        super(message);
+    }
+}
